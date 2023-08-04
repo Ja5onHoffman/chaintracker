31571fab2dc106c9fcf224f41728e14dfc2f8797
Jason <j@sonhoffman.com>
2023-08-04 09:03:06 -0500

    miles over and slide out button. slide out doesn't work

--- functions.py
+++ functions.py
@@ -19,9 +19,21 @@ def update_token(user, client):
             db.session.commit()
 
 def update_miles(user, client):
-    bikes = user.bikes
-    for bike in bikes:
-        strava_bike = client.get_gear(bike.id)
-        bike.miles_current = round(strava_bike.distance.to(ureg.mile).magnitude, 1)
-        print('miles updated')
+    try:
+        bikes = user.bikes
+        for bike in bikes:
+            strava_bike = client.get_gear(bike.id)
+            bike.miles_current = round(strava_bike.distance.to(ureg.mile).magnitude, 1)
+            print(f'Updated miles for bike {bike.id}: {bike.miles_current}')
+        
+        # Committing once after all updates
         db.session.commit()
+        
+        # Refresh the bikes to see the updated values
+        for bike in bikes:
+            db.session.refresh(bike)
+
+    except Exception as e:
+        print(f'An error occurred while updating miles: {e}')
+        db.session.rollback()
+
